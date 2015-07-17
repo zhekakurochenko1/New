c1c7f2c7bc508ca9d4b1aed90587925bb24ed494
root <root@devel.solvegen.org>
2015-07-17 05:28:33 -0500

    2st commit_add_module_Cars

---
+++ module/Cars/Module.php
@@ -0,0 +1,48 @@
+<?php
+namespace Cars;
+
+use Cars\Model\Cars;
+use Cars\Model\CarsTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'Cars\Model\CarsTable' =>  function($sm) {
+                    $tableGateway = $sm->get('CarsTableGateway');
+                    $table = new carsTable($tableGateway);
+                    return $table;
+                },
+                'CarsTableGateway' => function ($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Cars());
+                    return new TableGateway('cars', $dbAdapter, null, $resultSetPrototype);
+                },
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
